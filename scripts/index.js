72a37cb5af3bcf8fd559d0baf104324d1d4cf05c
Nitesh Kumar Goshwami <86410121+Nitesh-Goshwami@users.noreply.github.com>
2022-06-22 11:33:18 +0530

    updating

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,37 @@
+let screen = document.getElementById("screen");
+let screenValue = "";
 
+ const backspace = () => {
+    var val = document.getElementById("screen").value;
+    document.getElementById("screen").value = val.substring(0, val.length-1)
+ }
+let buttons = document.querySelectorAll("button");
+for (item of buttons) {
+    item.addEventListener("click", (e) => {
+        text = e.target.innerText
+        if (text == "x") {
+            text = "*";
+            screenValue += text;
+            screen.value = screenValue
+        } else if (text == "=") {
+            screen.value = eval(screenValue)
+        } else if (text == "C") {
+            screenValue = "";
+            screen.value = screenValue
+        } else if (text == "pow") {
+            text ="**2"
+            screenValue += text;
+            screen.value = screenValue
+        } else if (text == "Del") {
+            backspace();
+            console.log(screenValue)
+            console.log(typeof screenValue)
+            screenValue.split("");
+            console.log(screenValue)
+        }
+        else {
+            screenValue += text;
+            screen.value = screenValue
+        }
+    })
+}
